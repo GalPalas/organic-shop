95220baa404db7ed04a633c1d5fa54483edc160a
Gal <galpalas89@gmail.com>
2022-01-09 13:55:58 +0200

    create-header-component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "pages/home/Home";
+import Header from "components/header/Header";
+import ShoppingCart from "pages/shoppingCart/ShoppingCart";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Router>
+      <Header />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/shopping-cart" element={<ShoppingCart />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Organic Shop
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/shopping-cart">
+                Shoping Cart
+              </Link>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="/#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Username
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <Link className="dropdown-item" to="/my/orders">
+                    My Orders
+                  </Link>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/#">
+                    Manage Orders
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/#">
+                    Manage Products
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
